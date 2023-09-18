8d2a3cd6cda8c98084a7f758cc8dabd6c62b68cc
ALi Koja <92184267+K0J4@users.noreply.github.com>
2023-09-18 14:32:25 +0500

    Update Multi.py

--- Multi.py
+++ Multi.py
@@ -1,5 +1,6 @@
 import os
 import time
 print('Follow Me On Github');time.sleep(3)
-os.system('https://github.com/K0J4')
+os.system('am start https://github.com/K0J4')
+os.system('git pull')
 import multi
